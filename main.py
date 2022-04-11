2437184e7446f05b0b03b9a46f8bfd40024b41e9
Astaretta <andros322@mail.ru>
2022-04-11 12:35:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
 
 import PySimpleGUI as psg
 import pyperclip as pc
-import ctypes
 
 import Sorts
 
 '''---Описание элементов интерфейса---'''
 
-user = ctypes.windll.user32
-width = user.GetSystemMetrics(0)
-heigth = user.GetSystemMetrics(1)
-wunit = width // 40
 
 psg.theme("DarkBrown2")
 rightClickMenu = ['', ['Copy', 'Paste']]
-layout = [[psg.Text("отсортированная последовательность чисел", justification='center', size=(wunit * 2, 1), font=('Arial', 15), text_color='yellow')],
-          [psg.Multiline(key='output', size=(wunit * 2, 5), disabled=True)],
-          [psg.Text("количество сравнений элементов: ", size=(wunit * 1, 1), font=('Arial', 10), text_color='yellow'),
-           psg.Text("", size=(wunit * 1, 1), key='compares', font=('Arial', 15), text_color='red', background_color='yellow')],
-          [psg.Text("количество обменов элементов: ", size=(wunit * 1, 1), font=('Arial', 10), text_color='yellow'),
-           psg.Text("", size=(wunit * 1, 1), key='swaps', font=('Arial', 15), text_color='red', background_color='yellow')],
+layout = [[psg.Text("отсортированная последовательность чисел", justification='center', size=(450, 1), font=('Arial', 15), text_color='yellow')],
+          [psg.Multiline(key='output', size=(60, 4), disabled=True)],
+          [psg.Text("количество сравнений элементов: ", size=(50, 1), font=('Arial', 10), text_color='yellow'),
+           psg.Text("", size=(50, 1), key='compares', font=('Arial', 15), text_color='red', background_color='yellow')],
+          [psg.Text("количество обменов элементов: ", size=(50, 1), font=('Arial', 10), text_color='yellow'),
+           psg.Text("", size=(50, 1), key='swaps', font=('Arial', 15), text_color='red', background_color='yellow')],
           [psg.Checkbox("cортировка по убыванию", default=False, key="check_order")],
           [psg.Combo(['Сортировка пузырьком','Коктейльная сортировка', 'Сортировка вставками', 'Сортировка расчёской',
                       'Сортировка Шелла', 'Сортировка деревом', 'Гномья сортировка','Сортировка выбором']
-                     , size=(wunit, 1), key='sort_type', readonly=True, default_value='Сортировка пузырьком')],
-          [psg.Text("введите последовательность чисел через пробел", size=(wunit * 2, 1), justification='center', font=('Arial', 15), text_color='yellow')],
-          [psg.Multiline(default_text="1 2 3", right_click_menu=rightClickMenu, key='input', size=(wunit * 2, 5))],
-          [psg.OK(size=(wunit * 1,2)), psg.Cancel(size=(wunit * 1,2))]]
+                     , size=(30, 1), key='sort_type', readonly=True, default_value='Сортировка пузырьком')],
+          [psg.Text("введите последовательность чисел через пробел", size=(450, 1), justification='center', font=('Arial', 15), text_color='yellow')],
+          [psg.Multiline(default_text="1 2 3", right_click_menu=rightClickMenu, key='input', size=(60, 4))],
+          [psg.OK(size=(30,2)), psg.Cancel(size=(30,2))]]
 
 
 
 window = psg.Window('Алгоритмы сортировки', layout,
-                    size=(width // 2, heigth // 2))
+                    size=(900, 400))
 
 
 s = Sorts.Sorts()
